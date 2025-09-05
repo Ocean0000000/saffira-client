6a2b7a983aaa897c1c726e7e5e1788510cc2f658
Ocean Van <vantocean509@gmail.com>
2025-09-05 11:56:11 -0700

    SAFFIRA-5 - Update logout to go back to landing page

--- src/routes/(auth)/classes/+page.svelte
+++ src/routes/(auth)/classes/+page.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
 	import { logout } from '$lib/auth';
 	import type { PageProps } from './$types';
 
 	let { data }: PageProps = $props();
 
-	const handleLogout = async () => await logout();
+	const handleLogout = async () => {
+		await logout();
+		goto('/');
+	};
 </script>
 
-<button class="btn btn-primary" onclick={handleLogout}>Logout</button>
+<button class="btn btn-primary cursor-pointer" onclick={handleLogout}>Logout</button>
